7c2f4defa7bfa86005791d05b96df426e7fb0ecc
raquelsancheze <102796196+raquelsancheze@users.noreply.github.com>
2023-02-28 18:26:39 +0100

    Update CalcClient.java

--- CalcClient.java
+++ CalcClient.java
@@ -63,7 +63,7 @@ public class CalcClient {
                         }
                     }
                     else if(opt.equals("5")){
-                       pot.ptintln("El numero en decimal es: "+ calcImpl.decimalABinario(getFloat("a")));
+                       out.println("El numero en decimal es: "+ calcImpl.decimalABinario(getFloat("a")));
                     }
                 } catch (Exception e) {
                     out.println("===");
